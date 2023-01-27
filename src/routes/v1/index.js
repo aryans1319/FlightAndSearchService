692bfaf8ba3ca0abdbcfe2fff601f8a380d23e3e
Aryan <aryan4p130202@gmail.com>
2023-01-27 20:58:00 +0530

    final commit

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,26 +2,25 @@ const express = require('express');
 
 const { FlightMiddlewares } = require('../../middlewares/index');
 
-const CityController = require('../../controllers/city-contoller');
+const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
 const AirportController = require('../../controllers/airport-controller');
-
 const router = express.Router();
 
 router.post('/city', CityController.create);
 router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
-router.patch('/city/:id', CityController.update);
 router.get('/city', CityController.getAll);
+router.patch('/city/:id', CityController.update);
 
 router.post(
     '/flights', 
     FlightMiddlewares.validateCreateFlight, 
     FlightController.create
 );
-
-router.post('/flights', FlightController.create);
 router.get('/flights', FlightController.getAll);
+router.get('/flights/:id', FlightController.get);
+router.patch('/flights/:id', FlightController.update);
 
 router.post('/airports', AirportController.create);
 
